c916390b00174c09444aabcfcfc3aab78cefd726
Simar <simar@pop-os.localdomain>
2021-09-23 22:02:18 -0400

    User Input Improved

--- src/FileManage/Main.java
+++ src/FileManage/Main.java
@@ -5,11 +5,21 @@ import java.util.Scanner;
 public class Main {
 
     Main() {
-//        System.out.println("Do you want to manage files recursively (Y/N)? ");
-//        String flag = new Scanner(System.in).toString();
-//        /media/simar/TestFolder
-//        C:\Users\simar\Documents\Apple photos
-        Manage m = new Manage("/media/simar/TestFolder", "N", System.getProperty("os.name").toLowerCase());
+        System.out.println("Provide a directory to apply cleanup: ");
+        String path = new Scanner(System.in).nextLine();
+        while (path.contains(".")) {
+            System.out.println("Error: Not a directory.\nPlease provide a valid directory: ");
+            path = new Scanner(System.in).nextLine();
+        }
+
+        System.out.println("Do you wish to apply cleanup to all folders within the provided directory? (Y/N): ");
+        String flag = new Scanner(System.in).nextLine();
+        while (!(flag.toLowerCase().equals("n") || flag.toLowerCase().equals("y"))) {
+            System.out.println("Not a valid input.\nExpected (Y/N): ");
+            flag = new Scanner(System.in).nextLine();
+        }
+
+        Manage m = new Manage(path, flag, System.getProperty("os.name").toLowerCase());
     }
 
     public static void main(String[] args) {
